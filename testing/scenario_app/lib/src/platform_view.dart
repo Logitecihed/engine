Logitecihed/engine

commit e54dfe0c0419dbfe9ba29ebc9c55f83b35a1bd0a

Platform View implemenation for Metal (#11070)

shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -361,19 +361,13 @@ void FlutterPlatformViewsController::Reset() {
   views_to_recomposite_.clear();
 }
 
-bool FlutterPlatformViewsController::SubmitFrame(bool gl_rendering,
-                                                 GrContext* gr_context,
+bool FlutterPlatformViewsController::SubmitFrame(GrContext* gr_context,
                                                  std::shared_ptr<IOSGLContext> gl_context) {
   DisposeViews();
 
   bool did_submit = true;
-  for (size_t i = 0; i < composition_order_.size(); i++) {
-    int64_t view_id = composition_order_[i];
-    if (gl_rendering) {
-      EnsureGLOverlayInitialized(view_id, gl_context, gr_context);
-    } else {
-      EnsureOverlayInitialized(view_id);
-    }
+  for (int64_t view_id : composition_order_) {
+    EnsureOverlayInitialized(view_id, std::move(gl_context), gr_context);
     auto frame = overlays_[view_id]->surface->AcquireFrame(frame_size_);
     SkCanvas* canvas = frame->SkiaCanvas();
     canvas->drawPicture(picture_recorders_[view_id]->finishRecordingAsPicture());
@@ -455,46 +449,53 @@ void FlutterPlatformViewsController::DisposeViews() {
   views_to_dispose_.clear();
 }
 
-void FlutterPlatformViewsController::EnsureOverlayInitialized(int64_t overlay_id) {
-  if (overlays_.count(overlay_id) != 0) {
-    return;
-  }
-  FlutterOverlayView* overlay_view = [[FlutterOverlayView alloc] init];
-  overlay_view.frame = flutter_view_.get().bounds;
-  overlay_view.autoresizingMask =
-      (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
-  std::unique_ptr<IOSSurface> ios_surface = overlay_view.createSoftwareSurface;
-  std::unique_ptr<Surface> surface = ios_surface->CreateGPUSurface();
-  overlays_[overlay_id] = std::make_unique<FlutterPlatformViewLayer>(
-      fml::scoped_nsobject<UIView>(overlay_view), std::move(ios_surface), std::move(surface));
-}
-
-void FlutterPlatformViewsController::EnsureGLOverlayInitialized(
+void FlutterPlatformViewsController::EnsureOverlayInitialized(
     int64_t overlay_id,
     std::shared_ptr<IOSGLContext> gl_context,
     GrContext* gr_context) {
-  if (overlays_.count(overlay_id) != 0) {
+  FML_DCHECK(flutter_view_);
+
+  auto overlay_it = overlays_.find(overlay_id);
+
+  if (!gr_context) {
+    FML_DLOG(ERROR) << "No GrContext";
+    if (overlays_.count(overlay_id) != 0) {
+      return;
+    }
+    fml::scoped_nsobject<FlutterOverlayView> overlay_view(
+        [[[FlutterOverlayView alloc] init] retain]);
+    overlay_view.get().frame = flutter_view_.get().bounds;
+    overlay_view.get().autoresizingMask =
+        (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+    std::unique_ptr<IOSSurface> ios_surface = [overlay_view.get() createSurface:nil];
+    std::unique_ptr<Surface> surface = ios_surface->CreateGPUSurface();
+    overlays_[overlay_id] = std::make_unique<FlutterPlatformViewLayer>(
+        std::move(overlay_view), std::move(ios_surface), std::move(surface));
+    return;
+  }
+
+  if (overlay_it != overlays_.end()) {
     if (gr_context != overlays_gr_context_) {
       overlays_gr_context_ = gr_context;
       // The overlay already exists, but the GrContext was changed so we need to recreate
       // the rendering surface with the new GrContext.
-      IOSSurfaceGL* ios_surface_gl = (IOSSurfaceGL*)overlays_[overlay_id]->ios_surface.get();
-      std::unique_ptr<Surface> surface = ios_surface_gl->CreateSecondaryGPUSurface(gr_context);
-      overlays_[overlay_id]->surface = std::move(surface);
+      IOSSurface* ios_surface = overlay_it->second->ios_surface.get();
+      std::unique_ptr<Surface> surface = ios_surface->CreateGPUSurface(gr_context);
+      overlay_it->second->surface = std::move(surface);
     }
     return;
   }
   auto contentsScale = flutter_view_.get().layer.contentsScale;
-  FlutterOverlayView* overlay_view =
-      [[FlutterOverlayView alloc] initWithContentsScale:contentsScale];
-  overlay_view.frame = flutter_view_.get().bounds;
-  overlay_view.autoresizingMask =
+  fml::scoped_nsobject<FlutterOverlayView> overlay_view(
+      [[[FlutterOverlayView alloc] initWithContentsScale:contentsScale] retain]);
+  overlay_view.get().frame = flutter_view_.get().bounds;
+  overlay_view.get().autoresizingMask =
       (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
-  std::unique_ptr<IOSSurfaceGL> ios_surface =
-      [overlay_view createGLSurfaceWithContext:std::move(gl_context)];
-  std::unique_ptr<Surface> surface = ios_surface->CreateSecondaryGPUSurface(gr_context);
+  std::unique_ptr<IOSSurface> ios_surface =
+      [overlay_view.get() createSurface:std::move(gl_context)];
+  std::unique_ptr<Surface> surface = ios_surface->CreateGPUSurface(gr_context);
   overlays_[overlay_id] = std::make_unique<FlutterPlatformViewLayer>(
-      fml::scoped_nsobject<UIView>(overlay_view), std::move(ios_surface), std::move(surface));
+      std::move(overlay_view), std::move(ios_surface), std::move(surface));
   overlays_gr_context_ = gr_context;
 }
 

ci/licenses_golden/licenses_flutter
@@ -513,6 +513,7 @@ FILE: ../../../flutter/shell/common/vsync_waiter.cc
 FILE: ../../../flutter/shell/common/vsync_waiter.h
 FILE: ../../../flutter/shell/common/vsync_waiter_fallback.cc
 FILE: ../../../flutter/shell/common/vsync_waiter_fallback.h
+FILE: ../../../flutter/shell/gpu/gpu_surface_delegate.h
 FILE: ../../../flutter/shell/gpu/gpu_surface_gl.cc
 FILE: ../../../flutter/shell/gpu/gpu_surface_gl.h
 FILE: ../../../flutter/shell/gpu/gpu_surface_gl_delegate.cc

shell/common/animator.cc
@@ -27,14 +27,18 @@ Animator::Animator(Delegate& delegate,
       waiter_(std::move(waiter)),
       last_begin_frame_time_(),
       dart_frame_deadline_(0),
+#if FLUTTER_SHELL_ENABLE_METAL
+      layer_tree_pipeline_(fml::MakeRefCounted<LayerTreePipeline>(2)),
+#else   // FLUTTER_SHELL_ENABLE_METAL
       // TODO(dnfield): We should remove this logic and set the pipeline depth
-      // back to 2 in this case. See https://github.com/flutter/engine/pull/9132
-      // for discussion.
+      // back to 2 in this case. See
+      // https://github.com/flutter/engine/pull/9132 for discussion.
       layer_tree_pipeline_(fml::MakeRefCounted<LayerTreePipeline>(
           task_runners.GetPlatformTaskRunner() ==
                   task_runners.GetGPUTaskRunner()
               ? 1
               : 2)),
+#endif  // FLUTTER_SHELL_ENABLE_METAL
       pending_frame_semaphore_(1),
       frame_number_(1),
       paused_(false),
@@ -42,7 +46,8 @@ Animator::Animator(Delegate& delegate,
       frame_scheduled_(false),
       notify_idle_task_id_(0),
       dimension_change_pending_(false),
-      weak_factory_(this) {}
+      weak_factory_(this) {
+}
 
 Animator::~Animator() = default;
 

shell/gpu/BUILD.gn
@@ -27,6 +27,7 @@ source_set("gpu_surface_software") {
 
 source_set("gpu_surface_gl") {
   sources = [
+    "$gpu_dir/gpu_surface_delegate.h",
     "$gpu_dir/gpu_surface_gl.cc",
     "$gpu_dir/gpu_surface_gl.h",
     "$gpu_dir/gpu_surface_gl_delegate.cc",
@@ -50,6 +51,7 @@ source_set("gpu_surface_vulkan") {
 
 source_set("gpu_surface_metal") {
   sources = [
+    "$gpu_dir/gpu_surface_delegate.h",
     "$gpu_dir/gpu_surface_metal.h",
     "$gpu_dir/gpu_surface_metal.mm",
   ]

shell/gpu/gpu_surface_delegate.h
@@ -1,0 +1,18 @@
+#ifndef FLUTTER_SHELL_GPU_GPU_SURFACE_DELEGATE_H_
+#define FLUTTER_SHELL_GPU_GPU_SURFACE_DELEGATE_H_
+
+#include "flutter/flow/embedded_views.h"
+#include "flutter/fml/macros.h"
+
+namespace flutter {
+
+class GPUSurfaceDelegate {
+ public:
+  // Get a reference to the external views embedder. This happens on the same
+  // thread that the renderer is operating on.
+  virtual ExternalViewEmbedder* GetExternalViewEmbedder() = 0;
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_GPU_GPU_SURFACE_DELEGATE_H_

shell/gpu/gpu_surface_gl_delegate.h
@@ -7,12 +7,13 @@
 
 #include "flutter/flow/embedded_views.h"
 #include "flutter/fml/macros.h"
+#include "flutter/shell/gpu/gpu_surface_delegate.h"
 #include "third_party/skia/include/core/SkMatrix.h"
 #include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 
 namespace flutter {
 
-class GPUSurfaceGLDelegate {
+class GPUSurfaceGLDelegate : public GPUSurfaceDelegate {
  public:
   // Called to make the main GL context current on the current thread.
   virtual bool GLContextMakeCurrent() = 0;
@@ -41,10 +42,6 @@ class GPUSurfaceGLDelegate {
   // A transformation applied to the onscreen surface before the canvas is
   // flushed.
   virtual SkMatrix GLContextSurfaceTransformation() const;
-
-  // Get a reference to the external views embedder. This happens on the same
-  // thread that the renderer is operating on.
-  virtual ExternalViewEmbedder* GetExternalViewEmbedder() = 0;
 
   sk_sp<const GrGLInterface> GetGLInterface() const;
 

shell/gpu/gpu_surface_metal.h
@@ -10,6 +10,7 @@
 #include "flutter/fml/macros.h"
 #include "flutter/fml/platform/darwin/scoped_nsobject.h"
 #include "flutter/shell/common/surface.h"
+#include "flutter/shell/gpu/gpu_surface_delegate.h"
 #include "third_party/skia/include/gpu/GrContext.h"
 
 @class CAMetalLayer;
@@ -18,11 +19,15 @@ namespace flutter {
 
 class GPUSurfaceMetal : public Surface {
  public:
-  GPUSurfaceMetal(fml::scoped_nsobject<CAMetalLayer> layer);
+  GPUSurfaceMetal(GPUSurfaceDelegate* delegate, fml::scoped_nsobject<CAMetalLayer> layer);
+  GPUSurfaceMetal(GPUSurfaceDelegate* delegate,
+                  sk_sp<GrContext> gr_context,
+                  fml::scoped_nsobject<CAMetalLayer> layer);
 
   ~GPUSurfaceMetal() override;
 
  private:
+  GPUSurfaceDelegate* delegate_;
   fml::scoped_nsobject<CAMetalLayer> layer_;
   sk_sp<GrContext> context_;
   fml::scoped_nsprotocol<id<MTLCommandQueue>> command_queue_;
@@ -38,6 +43,9 @@ class GPUSurfaceMetal : public Surface {
 
   // |Surface|
   GrContext* GetContext() override;
+
+  // |Surface|
+  flutter::ExternalViewEmbedder* GetExternalViewEmbedder() override;
 
   // |Surface|
   bool MakeRenderContextCurrent() override;

shell/gpu/gpu_surface_metal.mm
@@ -12,8 +12,9 @@
 
 namespace flutter {
 
-GPUSurfaceMetal::GPUSurfaceMetal(fml::scoped_nsobject<CAMetalLayer> layer)
-    : layer_(std::move(layer)) {
+GPUSurfaceMetal::GPUSurfaceMetal(GPUSurfaceDelegate* delegate,
+                                 fml::scoped_nsobject<CAMetalLayer> layer)
+    : delegate_(delegate), layer_(std::move(layer)) {
   if (!layer_) {
     FML_LOG(ERROR) << "Could not create metal surface because of invalid layer.";
     return;
@@ -39,6 +40,32 @@ GPUSurfaceMetal::GPUSurfaceMetal(fml::scoped_nsobject<CAMetalLayer> layer)
   }
 
   context_ = context;
+}
+
+GPUSurfaceMetal::GPUSurfaceMetal(GPUSurfaceDelegate* delegate,
+                                 sk_sp<GrContext> gr_context,
+                                 fml::scoped_nsobject<CAMetalLayer> layer)
+    : delegate_(delegate), layer_(std::move(layer)), context_(gr_context) {
+  if (!layer_) {
+    FML_LOG(ERROR) << "Could not create metal surface because of invalid layer.";
+    return;
+  }
+  if (!context_) {
+    FML_LOG(ERROR) << "Could not create metal surface because of invalid Skia metal context.";
+    return;
+  }
+
+  layer.get().pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+  auto metal_device = fml::scoped_nsprotocol<id<MTLDevice>>([layer_.get().device retain]);
+  auto metal_queue = fml::scoped_nsprotocol<id<MTLCommandQueue>>([metal_device newCommandQueue]);
+
+  if (!metal_device || !metal_queue) {
+    FML_LOG(ERROR) << "Could not create metal device or queue.";
+    return;
+  }
+
+  command_queue_ = metal_queue;
 }
 
 GPUSurfaceMetal::~GPUSurfaceMetal() = default;
@@ -112,11 +139,26 @@ std::unique_ptr<SurfaceFrame> GPUSurfaceMetal::AcquireFrame(const SkISize& size)
     return nullptr;
   }
 
-  auto submit_callback = [drawable = next_drawable, command_buffer](
+  bool hasExternalViewEmbedder = delegate_->GetExternalViewEmbedder() != nullptr;
+
+  // External views need to present with transaction. When presenting with
+  // transaction, we have to block, otherwise we risk presenting the drawable
+  // after the CATransaction has completed.
+  // See:
+  // https://developer.apple.com/documentation/quartzcore/cametallayer/1478157-presentswithtransaction
+  // TODO(dnfield): only do this if transactions are actually being used.
+  // https://github.com/flutter/flutter/issues/24133
+  auto submit_callback = [drawable = next_drawable, command_buffer, hasExternalViewEmbedder](
                              const SurfaceFrame& surface_frame, SkCanvas* canvas) -> bool {
     canvas->flush();
-    [command_buffer.get() presentDrawable:drawable.get()];
-    [command_buffer.get() commit];
+    if (!hasExternalViewEmbedder) {
+      [command_buffer.get() presentDrawable:drawable.get()];
+      [command_buffer.get() commit];
+    } else {
+      [command_buffer.get() commit];
+      [command_buffer.get() waitUntilScheduled];
+      [drawable.get() present];
+    }
     return true;
   };
 
@@ -135,6 +177,11 @@ SkMatrix GPUSurfaceMetal::GetRootTransformation() const {
 // |Surface|
 GrContext* GPUSurfaceMetal::GetContext() {
   return context_.get();
+}
+
+// |Surface|
+flutter::ExternalViewEmbedder* GPUSurfaceMetal::GetExternalViewEmbedder() {
+  return delegate_->GetExternalViewEmbedder();
 }
 
 // |Surface|

commit 166b841933f5b6a6520ba36c425d9fa330b34ca4

Fix iOS crash when multiple platform views are in the scene (#13449)
Having 2 or more platform views simultaneously in the layer tree was crashing immediately on iOS with GL backend.

This regressed in #11070 which passed gl_context to a function in a loop using std::move (which meant on the second iteration the caller is no longer the owner of the field).

I added a scenarios_app test, though this test doesn't run on a physical device on CI so it would have only caught the problem when running locally (flutter/flutter#43852).

shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -368,7 +368,7 @@ bool FlutterPlatformViewsController::SubmitFrame(GrContext* gr_context,
 
   bool did_submit = true;
   for (int64_t view_id : composition_order_) {
-    EnsureOverlayInitialized(view_id, std::move(gl_context), gr_context);
+    EnsureOverlayInitialized(view_id, gl_context, gr_context);
     auto frame = overlays_[view_id]->surface->AcquireFrame(frame_size_);
     SkCanvas* canvas = frame->SkiaCanvas();
     canvas->drawPicture(picture_recorders_[view_id]->finishRecordingAsPicture());

testing/scenario_app/ios/Scenarios/Scenarios.xcodeproj/project.pbxproj
@@ -30,6 +30,7 @@
 		24D47D1B230C79840069DD5E /* golden_platform_view_D211AP.png in Resources */ = {isa = PBXBuildFile; fileRef = 24D47D1A230C79840069DD5E /* golden_platform_view_D211AP.png */; };
 		24D47D1D230CA2700069DD5E /* golden_platform_view_iPhone SE_simulator.png in Resources */ = {isa = PBXBuildFile; fileRef = 24D47D1C230CA2700069DD5E /* golden_platform_view_iPhone SE_simulator.png */; };
 		24F1FB89230B4579005ACE7C /* TextPlatformView.m in Sources */ = {isa = PBXBuildFile; fileRef = 24F1FB87230B4579005ACE7C /* TextPlatformView.m */; };
+		59A97FD8236A49D300B4C066 /* golden_platform_view_multiple_iPhone SE_simulator.png in Resources */ = {isa = PBXBuildFile; fileRef = 59A97FD7236A49D300B4C066 /* golden_platform_view_multiple_iPhone SE_simulator.png */; };
 		6816DB9E231750ED00A51400 /* GoldenPlatformViewTests.m in Sources */ = {isa = PBXBuildFile; fileRef = 6816DB9D231750ED00A51400 /* GoldenPlatformViewTests.m */; };
 		6816DBA12317573300A51400 /* GoldenImage.m in Sources */ = {isa = PBXBuildFile; fileRef = 6816DBA02317573300A51400 /* GoldenImage.m */; };
 		6816DBA42318358200A51400 /* PlatformViewGoldenTestManager.m in Sources */ = {isa = PBXBuildFile; fileRef = 6816DBA32318358200A51400 /* PlatformViewGoldenTestManager.m */; };
@@ -121,6 +122,7 @@
 		24D47D1E230CA4480069DD5E /* README.md */ = {isa = PBXFileReference; lastKnownFileType = net.daringfireball.markdown; path = README.md; sourceTree = "<group>"; };
 		24F1FB87230B4579005ACE7C /* TextPlatformView.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = TextPlatformView.m; sourceTree = "<group>"; };
 		24F1FB88230B4579005ACE7C /* TextPlatformView.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TextPlatformView.h; sourceTree = "<group>"; };
+		59A97FD7236A49D300B4C066 /* golden_platform_view_multiple_iPhone SE_simulator.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "golden_platform_view_multiple_iPhone SE_simulator.png"; sourceTree = "<group>"; };
 		6816DB9C231750ED00A51400 /* GoldenPlatformViewTests.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = GoldenPlatformViewTests.h; sourceTree = "<group>"; };
 		6816DB9D231750ED00A51400 /* GoldenPlatformViewTests.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = GoldenPlatformViewTests.m; sourceTree = "<group>"; };
 		6816DB9F2317573300A51400 /* GoldenImage.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = GoldenImage.h; sourceTree = "<group>"; };
@@ -216,6 +218,7 @@
 		248D76ED22E388380012F0C1 /* ScenariosUITests */ = {
 			isa = PBXGroup;
 			children = (
+				59A97FD7236A49D300B4C066 /* golden_platform_view_multiple_iPhone SE_simulator.png */,
 				244EA6CF230DBE8900B2D26E /* golden_platform_view_D21AP.png */,
 				24D47D1C230CA2700069DD5E /* golden_platform_view_iPhone SE_simulator.png */,
 				24D47D1A230C79840069DD5E /* golden_platform_view_D211AP.png */,
@@ -375,6 +378,7 @@
 				6816DBAA2318696600A51400 /* golden_platform_view_clippath_iPhone SE_simulator.png in Resources */,
 				6816DBAD2318696600A51400 /* golden_platform_view_cliprect_iPhone SE_simulator.png in Resources */,
 				24D47D1B230C79840069DD5E /* golden_platform_view_D211AP.png in Resources */,
+				59A97FD8236A49D300B4C066 /* golden_platform_view_multiple_iPhone SE_simulator.png in Resources */,
 				24D47D1D230CA2700069DD5E /* golden_platform_view_iPhone SE_simulator.png in Resources */,
 				244EA6D0230DBE8900B2D26E /* golden_platform_view_D21AP.png in Resources */,
 				6816DBAC2318696600A51400 /* golden_platform_view_opacity_iPhone SE_simulator.png in Resources */,

testing/scenario_app/ios/Scenarios/Scenarios/AppDelegate.m
@@ -28,6 +28,7 @@
   // The launchArgsMap should match the one in the `PlatformVieGoldenTestManager`.
   NSDictionary<NSString*, NSString*>* launchArgsMap = @{
     @"--platform-view" : @"platform_view",
+    @"--platform-view-multiple" : @"platform_view_multiple",
     @"--platform-view-cliprect" : @"platform_view_cliprect",
     @"--platform-view-cliprrect" : @"platform_view_cliprrect",
     @"--platform-view-clippath" : @"platform_view_clippath",

testing/scenario_app/ios/Scenarios/ScenariosUITests/PlatformViewGoldenTestManager.m
@@ -23,6 +23,7 @@ NSDictionary* launchArgsMap;
     dispatch_once(&onceToken, ^{
       launchArgsMap = @{
         @"--platform-view" : @"platform_view",
+        @"--platform-view-multiple" : @"platform_view_multiple",
         @"--platform-view-cliprect" : @"platform_view_cliprect",
         @"--platform-view-cliprrect" : @"platform_view_cliprrect",
         @"--platform-view-clippath" : @"platform_view_clippath",

testing/scenario_app/ios/Scenarios/ScenariosUITests/PlatformViewUITests.m
@@ -22,6 +22,24 @@
 
 @end
 
+@interface MultiplePlatformViewsTest : GoldenPlatformViewTests
+
+@end
+
+@implementation MultiplePlatformViewsTest
+
+- (instancetype)initWithInvocation:(NSInvocation*)invocation {
+  PlatformViewGoldenTestManager* manager =
+      [[PlatformViewGoldenTestManager alloc] initWithLaunchArg:@"--platform-view-multiple"];
+  return [super initWithManager:manager invocation:invocation];
+}
+
+- (void)testPlatformView {
+  [self checkGolden];
+}
+
+@end
+
 // Clip Rect Tests
 @interface PlatformViewMutationClipRectTests : GoldenPlatformViewTests
 

testing/scenario_app/lib/main.dart
@@ -22,6 +22,7 @@ Map<String, Scenario> _scenarios = <String, Scenario>{
   'platform_view_clippath': PlatformViewClipPathScenario(window, 'PlatformViewClipPath', id: 3),
   'platform_view_transform': PlatformViewTransformScenario(window, 'PlatformViewTransform', id: 4),
   'platform_view_opacity': PlatformViewOpacityScenario(window, 'PlatformViewOpacity', id: 5),
+  'platform_view_multiple': MultiPlatformViewScenario(window, firstId: 6, secondId: 7),
   'poppable_screen': PoppableScreenScenario(window),
 };
 

testing/scenario_app/lib/src/platform_view.dart
@@ -34,7 +34,7 @@ class PlatformViewScenario extends Scenario with _BasePlatformViewScenarioMixin 
   PlatformViewScenario(Window window, String text, {int id = 0})
       : assert(window != null),
         super(window) {
-    constructScenario(window, text, id);
+    createPlatformView(window, text, id);
   }
 
   @override
@@ -47,13 +47,45 @@ class PlatformViewScenario extends Scenario with _BasePlatformViewScenarioMixin 
   }
 }
 
+/// Builds a scene with 2 platform views.
+class MultiPlatformViewScenario extends Scenario with _BasePlatformViewScenarioMixin {
+  /// Creates the PlatformView scenario.
+  ///
+  /// The [window] parameter must not be null.
+  MultiPlatformViewScenario(Window window, {this.firstId, this.secondId})
+      : assert(window != null),
+        super(window) {
+    createPlatformView(window, 'platform view 1', firstId);
+    createPlatformView(window, 'platform view 2', secondId);
+  }
+
+  /// The platform view identifier to use for the first platform view.
+  final int firstId;
+
+  /// The platform view identifier to use for the second platform view.
+  final int secondId;
+
+  @override
+  void onBeginFrame(Duration duration) {
+    final SceneBuilder builder = SceneBuilder();
+
+    builder.pushOffset(0, 0);
+
+    builder.pushOffset(0, 600);
+    _addPlatformViewtoScene(builder, firstId, 500, 500);
+    builder.pop();
+
+    finishBuilderByAddingPlatformViewAndPicture(builder, secondId);
+  }
+}
+
 /// Platform view with clip rect.
 class PlatformViewClipRectScenario extends Scenario with _BasePlatformViewScenarioMixin {
   /// Constructs a platform view with clip rect scenario.
   PlatformViewClipRectScenario(Window window, String text, {int id = 0})
       : assert(window != null),
         super(window) {
-    constructScenario(window, text, id);
+    createPlatformView(window, text, id);
   }
 
   @override
@@ -165,7 +197,7 @@ mixin _BasePlatformViewScenarioMixin on Scenario {
   /// It prepare a TextPlatformView so it can be added to the SceneBuilder in `onBeginFrame`.
   /// Call this method in the constructor of the platform view related scenarios
   /// to perform necessary set up.
-  void constructScenario(Window window, String text, int id) {
+  void createPlatformView(Window window, String text, int id) {
     const int _valueInt32 = 3;
     const int _valueFloat64 = 6;
     const int _valueString = 7;
@@ -227,15 +259,19 @@ mixin _BasePlatformViewScenarioMixin on Scenario {
     );
   }
 
-  // Add a platform view and a picture to the scene, then finish the `sceneBuilder`.
-  void finishBuilderByAddingPlatformViewAndPicture(SceneBuilder sceneBuilder, int viewId) {
+  void _addPlatformViewtoScene(SceneBuilder sceneBuilder, int viewId, double width, double height) {
     if (Platform.isIOS) {
-      sceneBuilder.addPlatformView(viewId, width: 500, height: 500);
+      sceneBuilder.addPlatformView(viewId, width: width, height: height);
     } else if (Platform.isAndroid && _textureId != null) {
-      sceneBuilder.addTexture(_textureId, offset: const Offset(150, 300), width: 500, height: 500);
+      sceneBuilder.addTexture(_textureId, offset: const Offset(150, 300), width: width, height: height);
     } else {
       throw UnsupportedError('Platform ${Platform.operatingSystem} is not supported');
     }
+  }
+
+  // Add a platform view and a picture to the scene, then finish the `sceneBuilder`.
+  void finishBuilderByAddingPlatformViewAndPicture(SceneBuilder sceneBuilder, int viewId) {
+    _addPlatformViewtoScene(sceneBuilder, viewId, 500, 500);
     final PictureRecorder recorder = PictureRecorder();
     final Canvas canvas = Canvas(recorder);
     canvas.drawCircle(